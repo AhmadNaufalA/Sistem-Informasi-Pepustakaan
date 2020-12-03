d1e82acc6d1e8fed69ea33c0b161df23a7fdb37e
Ahmad Naufal Athaya <49048636+AhmadNaufalA@users.noreply.github.com>
2020-12-03 08:04:36 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'tugasta';
+$databaseUsername = 'root';
+$databasePassword = '';
+$koneksi = mysqli_connect("localhost","root","","tugasta");
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+
+
+?>
